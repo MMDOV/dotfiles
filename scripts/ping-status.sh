f991b453114cc3e8c79fc4af752b6f1c4e577712
mamadv <mohammadvary@gmail.com>
2025-05-02 11:45:29 +0330

    something

--- scripts/ping-status.sh
+++ scripts/ping-status.sh
@@ -4,7 +4,7 @@
 PING=$(ping -c 1 8.8.8.8 | grep 'time=' | awk -F'time=' '{print $2}' | awk '{print $1}')
 
 if [ -n "$PING" ]; then
-    echo "📡 ${PING}ms"
+    echo "${PING}ms"
 else
-    echo "📡 N/A"
+    echo "N/A"
 fi
