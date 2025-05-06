d3b4a511b64e05c152687a206bc96b3bf8821ada
mamadv <mohammadvary@gmail.com>
2025-05-06 08:02:09 +0330

    fixes

--- scripts/pacman.sh
+++ scripts/pacman.sh
@@ -7,5 +7,10 @@ if [ "$EUID" -ne 0 ]; then
     echo "Please run as root (e.g. with sudo)"
     exit 1
 fi
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 install -Dm644 $scripts/../system/pacman.conf /etc/pacman.conf
