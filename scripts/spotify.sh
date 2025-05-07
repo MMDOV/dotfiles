3a9410496b65a1ca224143b2af44e53d3a1cdfd0
mamadv <mohammadvary@gmail.com>
2025-05-07 16:19:16 +0330

    added spotify theme

--- scripts/spotify.sh
+++ scripts/spotify.sh
@@ -22,4 +22,5 @@ bash <(curl -sSL https://spotx-official.github.io/run.sh) -f
 # install and apply spicetify
 curl -fsSL https://raw.githubusercontent.com/spicetify/cli/main/install.sh | sh
 cp -rf "$HOME/personal/.config/spicetify/" "$HOME/.config/"
+curl -fsSL https://raw.githubusercontent.com/NYRI4/Comfy-spicetify/main/install.sh | sh
 spicetify apply
