1fda0afdc3aa0e40ce10a35177f3fd9d9a8c5bdd
mamadv <mohammadvary@gmail.com>
2025-05-08 10:52:50 +0330

    fixes

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -5,7 +5,7 @@ sudo pacman -S --noconfirm --needed tmux git bash bc coreutils jq playerctl
 
 # Copy tmux config
 cp -f $HOME/personal/home/.tmux.conf $HOME/.tmux.conf
-cp -rf $HOME/personal/.tmux/ $HOME/.tmux/plugins/
+cp -rf $HOME/personal/tmux/ $HOME/.tmux/plugins/
 
 # Install tmux package manager
 [ -d ~/.tmux/plugins/tpm ] || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
