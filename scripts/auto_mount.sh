c0d356ab36a405effac33adbe888d5925e81575e
mamadv <mohammadvary@gmail.com>
2025-05-11 22:30:18 +0330

    fixes

--- scripts/auto_mount.sh
+++ scripts/auto_mount.sh
@@ -1,23 +1,19 @@
 #!/usr/bin/env bash
 
 USBNAME=$(lsblk -o NAME,TRAN | awk '$2 == "usb" || $2 == "mmc" {print $1}' | grep -v "└─\|├─" | head -n 1)
-echo $USBNAME
 
 MOUNTROW=$(lsblk -o NAME,LABEL | grep -E "${USBNAME}p?[0-9]" | grep -v "${USBNAME}$" | sed 's/^[├└]─//' | head -n 1)
-echo $MOUNTROW
 
 if [ -n "$MOUNTROW" ]; then
     MOUNTNAME=$(echo "$MOUNTROW" | awk '{print $1}')
-    echo $MOUNTNAME
+
     MOUNT_OUTPUT=$(udisksctl mount -b "/dev/$MOUNTNAME" 2>&1)
-    echo $MOUNT_OUTPUT
 
     if echo "$MOUNT_OUTPUT" | grep -q "AlreadyMounted"; then
         MOUNTPOINT=$(lsblk -o NAME,MOUNTPOINT | grep "$MOUNTNAME" | awk '{print $2}')
     else
         MOUNTPOINT=$(echo "$MOUNT_OUTPUT" | grep "at" | awk '{print $NF}')
     fi
-    echo $MOUNTPOINT
 else
     echo "No mountable partition found for $USBNAME"
     exit 1
