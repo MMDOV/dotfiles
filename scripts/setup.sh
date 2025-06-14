2cf697ee428deb7dead13778cb76ae746c4a272f
mmdov <mohammadvary@gmail.com>
2025-06-14 20:09:46 +0330

    added thunar

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -83,6 +83,10 @@ print_msg "Installing yazi"
 chmod +x "$scripts/yazi.sh"
 $scripts/yazi.sh
 
+# Install thunar
+print_msg "Installing thunar"
+paru -S --noconfirm --needed thunar thunar-volman gvfs gvfs-mtp
+
 # install extra apps
 paru -S --noconfirm --needed zen-browser-bin firefox thunderbird chatterino2-git telegram-desktop discord torguard aria2
 paru -S --noconfirm --needed btop espeakup gimp libreoffice-still remmina virt-manager zathura zathura-pdf-mupdf mpv
