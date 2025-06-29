70744cf91381fc74335569b9b97ab89bd73aee8e
mmdov <mohammadvary@gmail.com>
2025-06-29 13:48:38 +0330

    added easyeffects

--- scripts/pipewire.sh
+++ scripts/pipewire.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sudo pacman -S --noconfirm --needed pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse wireplumber
-paru -S --noconfirm --needed pavucontrol 
+paru -S --noconfirm --needed pavucontrol easyeffects
