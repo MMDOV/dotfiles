f43e3f118d7657e4e003d33c79154d384ca57939
mmdov <mohammadvary@gmail.com>
2025-07-10 17:10:57 +0330

    fixes

--- scripts/lutris.sh
+++ scripts/lutris.sh
@@ -12,11 +12,19 @@ lib32-gnutls \
 lib32-mesa-libgl \
 lib32-vkd3d \
 lib32-vulkan-icd-loader \
+lib32-libldap \
+lib32-libpulse \
+lib32-alsa-plugins \
+lib32-openal \
+lib32-mesa \
+lib32-nvidia-utils \
 python-pefile \
 python-protobuf \
 vkd3d \
 vulkan-icd-loader \
 vulkan-tools \
 wine \
+wine-mono \
+wine-gecko \
 winetricks \
-xorg-xgamma \
+xorg-xgamma
