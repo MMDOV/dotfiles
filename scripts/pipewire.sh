0dce0e53448b108e7fab248f54e5fcc4bdf4ef62
mmdov <mohammadvary@gmail.com>
2025-08-06 13:13:40 +0330

    added pulsemixer

--- scripts/pipewire.sh
+++ scripts/pipewire.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sudo pacman -S --noconfirm --needed pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse wireplumber
-paru -S --noconfirm --needed pavucontrol easyeffects
+paru -S --noconfirm --needed pavucontrol easyeffects pulsemixer 
