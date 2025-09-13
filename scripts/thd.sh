8fcffc43a439fee3fed73e1072b248df0caa0e44
mmdov <mohammadvary@gmail.com>
2025-09-14 00:32:57 +0330

    fixes

--- scripts/thd.sh
+++ scripts/thd.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+CMD="/usr/sbin/thd --triggers $HOME/.config/triggerhappy/triggers.d/ --deviceglob /dev/input/event*"
+
+COUNT=0
 while true; do
-    while [ ! -e /dev/input/event26 ]; do
-        sleep 1
-    done
-
-    /usr/sbin/thd --triggers ~/.config/triggerhappy/triggers.d/ --deviceglob /dev/input/event*
-
+    NEWCOUNT=$(ls /dev/input/event* 2>/dev/null | wc -l)
+    if [[ $NEWCOUNT -ne $COUNT ]]; then
+        echo "Device count changed, restarting thd..."
+        pkill -x thd
+        $CMD &
+        COUNT=$NEWCOUNT
+    fi
     sleep 1
 done
