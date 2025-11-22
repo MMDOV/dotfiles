51dc3da3446a951604a25337a3e3aec207aaf037
mmdov <mohammadvary@gmail.com>
2025-11-23 01:06:22 +0330

    fixes

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,7 +2,6 @@ vim.g.snacks_animate = false
 
 vim.o.updatetime = 50
 
-vim.diagnostic.config({ virtual_text = false })
 vim.g.have_nerd_font = true
 vim.o.swapfile = false
 vim.o.backup = false
@@ -13,4 +12,3 @@ vim.opt.isfname:append("@-@")
 vim.o.colorcolumn = "80"
 vim.o.breakindent = true
 vim.o.ttimeoutlen = 10
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
