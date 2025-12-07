283c8de6353df46a4f956328527edd1e0e3d7dd5
mmdov <mohammadvary@gmail.com>
2025-12-07 05:31:52 +0330

    added youtube music .desktop

--- scripts/browsers.sh
+++ scripts/browsers.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 # Make sure paru is installed
 if ! command -v paru &>/dev/null; then
   print_msg "Installing Paru"
@@ -11,4 +13,5 @@ fi
 
 paru -S --noconfirm --needed zen-browser-bin firefox chromium
 
-install -Dm644 $HOME/personal/local/share/chromium.desktop /usr/share/applications/chromium.desktop
+install -Dm644 $BASE_DIR/../local/share/chromium.desktop /usr/share/applications/chromium.desktop
+cp -f $BASE_DIR/../local/share/youtube-music.desktop $HOME/.local/share/applications/youtube-music.desktop
