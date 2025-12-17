52431d1227bf5ac8d4171fe4fcd7eb695201e642
mmdov <mohammadvary@gmail.com>
2025-12-17 03:46:30 +0330

    fixes

--- scripts/paru.sh
+++ scripts/paru.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 if ! command -v paru &>/dev/null; then
-    sudo pacman -S --noconfirm --needed base-devel
-    mkdir -p $HOME/.parupack
-    git clone https://aur.archlinux.org/paru.git $HOME/.parupack
-    CURRENTDIR=$(pwd)
-    cd $HOME/.parupack
-    makepkg -si
-    cd "$CURRENTDIR"
+  sudo pacman -S --noconfirm --needed base-devel bat
+  mkdir -p $HOME/.parupack
+  git clone https://aur.archlinux.org/paru.git $HOME/.parupack
+  CURRENTDIR=$(pwd)
+  cd $HOME/.parupack
+  makepkg -si
+  cd "$CURRENTDIR"
 fi
