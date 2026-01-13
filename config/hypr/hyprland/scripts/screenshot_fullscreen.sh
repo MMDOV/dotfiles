63e08163a2ed5b814f4f32dc3966477a299f2f8c
mmdov <mohammadvary@gmail.com>
2026-01-13 17:24:29 +0330

    fixed the fullscreen screenshot

---
+++ config/hypr/hyprland/scripts/screenshot_fullscreen.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+monitors_count=$(hyprctl monitors | grep -Eoc "[0-9]*x[0-9]*@[0-9]*.[0-9]*\s+at\s+[0-9]*x[0-9]*")
+
+if [[ $monitors_count -eq 1 ]]; then
+  grim - | swappy -f -
+  exit 0
+fi
+
+monitors=$(hyprctl monitors | grep -Eo --group-separator=\n "[0-9]*x[0-9]*@[0-9]*.[0-9]*\s+at\s+[0-9]*x[0-9]*")
+
+selected_monitor=$(printf "Both\n$monitors" | fuzzel --dmenu --prompt "Which monitor?")
+
+if [[ $selected_monitor == "Both" ]]; then
+  grim - | swappy -f -
+  exit 0
+fi
+
+resolution=$(printf "$selected_monitor" | grep -Eo "[0-9]*x[0-9]*@" | grep -Eo "[0-9]*x[0-9]*")
+positions=$(printf "$selected_monitor" | grep -Eo "\s[0-9]*x[0-9]*" | grep -Eo "[0-9]*x[0-9]*")
+positions_1=$(printf "$positions" | grep -Eo "[0-9]*x" | grep -Eo "[0-9]*")
+positions_2=$(printf "$positions" | grep -Eo "x[0-9]*" | grep -Eo "[0-9]*")
+
+echo $positions_1,$positions_2
+
+grim -g "$positions_1,$positions_2 $(printf $resolution)" - | swappy -f -
