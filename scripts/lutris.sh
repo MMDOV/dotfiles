ba975b7b0d17080062a9cc9928304ac0ccd53a7b
mmdov <mohammadvary@gmail.com>
2026-02-16 15:21:38 +0330

    fixes

--- scripts/lutris.sh
+++ scripts/lutris.sh
@@ -1,31 +1,30 @@
 #!/usr/bin/env bash
 
 sudo pacman -S --needed \
-lutris \
-fluidsynth \
-gamemode \
-gvfs \
-innoextract \
-libayatana-appindicator \
-lib32-gamemode \
-lib32-gnutls \
-lib32-mesa-libgl \
-lib32-vkd3d \
-lib32-vulkan-icd-loader \
-lib32-libldap \
-lib32-libpulse \
-lib32-alsa-plugins \
-lib32-openal \
-lib32-mesa \
-lib32-nvidia-utils \
-python-pefile \
-python-protobuf \
-vkd3d \
-vulkan-icd-loader \
-vulkan-tools \
-wine \
-wine-mono \
-wine-gecko \
-winetricks \
-xorg-xgamma \
-umu-launcher
+  lutris \
+  fluidsynth \
+  gamemode \
+  gvfs \
+  innoextract \
+  libayatana-appindicator \
+  lib32-gamemode \
+  lib32-gnutls \
+  lib32-mesa-libgl \
+  lib32-vkd3d \
+  lib32-vulkan-icd-loader \
+  lib32-libldap \
+  lib32-libpulse \
+  lib32-alsa-plugins \
+  lib32-openal \
+  lib32-mesa \
+  python-pefile \
+  python-protobuf \
+  vkd3d \
+  vulkan-icd-loader \
+  vulkan-tools \
+  wine \
+  wine-mono \
+  wine-gecko \
+  winetricks \
+  xorg-xgamma \
+  umu-launcher
