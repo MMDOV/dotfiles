c48f85f104751eb7da8050964695760504a66e76
mmdov <mohammadvary@gmail.com>
2026-03-25 19:20:36 +0330

    Automated Commit - 2026-03-25 19:20:36

--- scripts/ping-status.sh
+++ scripts/ping-status.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 HISTFILE="/tmp/waybar_ping_history"
 
-PING=$(ping -c 1 8.8.8.8 | grep 'time=' | awk -F'time=' '{print $2}' | awk '{print $1}')
+PING=$(ping -c 1 192.168.1.1 | grep 'time=' | awk -F'time=' '{print $2}' | awk '{print $1}')
 
 if [ -n "$PING" ]; then
-    VALUE="${PING}ms"
+  VALUE="${PING}ms"
 else
-    VALUE="N/A"
+  VALUE="N/A"
 fi
 
-echo "$VALUE" >> "$HISTFILE"
-tail -n 5 "$HISTFILE" > "$HISTFILE.tmp" && mv "$HISTFILE.tmp" "$HISTFILE"
+echo "$VALUE" >>"$HISTFILE"
+tail -n 5 "$HISTFILE" >"$HISTFILE.tmp" && mv "$HISTFILE.tmp" "$HISTFILE"
 
 TOOLTIP=$(tac "$HISTFILE" | paste -sd '\n' - | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')
 
 printf '{"text":"%s","tooltip":"%s"}\n' "$VALUE" "$TOOLTIP"
-
