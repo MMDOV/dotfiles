6226b302f2666480ec15177ace9a602fc776a583
mmdov <mohammadvary@gmail.com>
2026-05-22 21:44:13 +0330

    Automated Commit - 2026-05-22 21:44:13

---
+++ config/cmus/lib.pl
@@ -0,0 +1,270 @@
+/home/mmdov/Music/Melodify/Lonely Akon.mp3
+/home/mmdov/Music/Melodify/Bi Gonah Alireza Ghorbani.mp3
+/mnt/HDD/Downloads/andra_day_rise_up.mp3
+/home/mmdov/Music/Melodify/Guns for Hire from the series Arcane League of Legends Woodkid, Arcane, League of Legends.mp3
+/home/mmdov/Music/Melodify/Ma Meilleure Ennemie from the series Arcane League of Legends Stromae, Pomme.mp3
+/home/mmdov/Music/Melodify/Voilà Barbara Pravi.mp3
+/home/mmdov/Music/Melodify/Bad Guy Billie Eilish.mp3
+/home/mmdov/Music/Melodify/Lovely Billie Eilish.mp3
+/home/mmdov/Music/Melodify/I Dont Wanna Be You Anymore Billie Eilish.mp3
+/home/mmdov/Music/Melodify/ocean eyes Billie Eilish.mp3
+/home/mmdov/Music/Melodify/everything i wanted Billie Eilish.mp3
+/home/mmdov/Music/Melodify/Getting Older Billie Eilish.mp3
+/home/mmdov/Music/Melodify/I Didnt Change My Number Billie Eilish.mp3
+/home/mmdov/Music/Melodify/Billie Bossa Nova Billie Eilish.mp3
+/home/mmdov/Music/Melodify/Everybody Dies Billie Eilish.mp3
+/home/mmdov/Music/Melodify/Your Power Billie Eilish.mp3
+/home/mmdov/Music/Melodify/Happier Than Ever Billie Eilish.mp3
+/home/mmdov/Music/Melodify/NDA Billie Eilish.mp3
+/home/mmdov/Music/Melodify/Therefore I Am Billie Eilish.mp3
+/home/mmdov/Music/Melodify/when the partys over Billie Eilish.mp3
+/home/mmdov/Music/Melodify/bury a friend Billie Eilish.mp3
+/home/mmdov/Music/Melodify/ilomilo Billie Eilish.mp3
+/home/mmdov/Music/Melodify/i love you Billie Eilish.mp3
+/home/mmdov/Music/Melodify/lovely Billie Eilish, Khalid.mp3
+/home/mmdov/Music/Melodify/hotline edit Billie Eilish.mp3
+/home/mmdov/Music/Melodify/What Was I Made For From The Motion Picture Barbie Billie Eilish.mp3
+/home/mmdov/Music/HIT ME HARD AND SOFT [Flac]/SKINNY - Billie Eilish (FLAC).flac
+/home/mmdov/Music/HIT ME HARD AND SOFT [Flac]/LUNCH - Billie Eilish (FLAC).flac
+/home/mmdov/Music/HIT ME HARD AND SOFT [Flac]/CHIHIRO - Billie Eilish (FLAC).flac
+/home/mmdov/Music/HIT ME HARD AND SOFT [Flac]/BIRDS OF A FEATHER - Billie Eilish (FLAC).flac
+/home/mmdov/Music/HIT ME HARD AND SOFT [Flac]/WILDFLOWER - Billie Eilish (FLAC).flac
+/home/mmdov/Music/HIT ME HARD AND SOFT [Flac]/THE GREATEST - Billie Eilish (FLAC).flac
+/home/mmdov/Music/HIT ME HARD AND SOFT [Flac]/L AMOUR DE MA VIE - Billie Eilish (FLAC).flac
+/home/mmdov/Music/HIT ME HARD AND SOFT [Flac]/THE DINER - Billie Eilish (FLAC).flac
+/home/mmdov/Music/HIT ME HARD AND SOFT [Flac]/BITTERSUITE - Billie Eilish (FLAC).flac
+/home/mmdov/Music/HIT ME HARD AND SOFT [Flac]/BLUE - Billie Eilish (FLAC).flac
+/home/mmdov/Music/Melodify/Toxic Britney Spears.mp3
+/home/mmdov/Music/Melodify/Oops...I Did It Again Britney Spears.mp3
+/home/mmdov/Music/Melodify/Stronger Britney Spears.mp3
+/home/mmdov/Music/Melodify/Criminal Britney Spears.mp3
+/home/mmdov/Music/Melodify/Grenade Bruno Mars.mp3
+/home/mmdov/Music/Melodify/The Lazy Song Bruno Mars.mp3
+/home/mmdov/Music/Melodify/Talking to the Moon Bruno Mars.mp3
+/home/mmdov/Music/Melodify/It Will Rain Bruno Mars.mp3
+/home/mmdov/Music/Melodify/When I Was Your Man Bruno Mars.mp3
+/home/mmdov/Music/Melodify/Locked out of Heaven Bruno Mars.mp3
+/home/mmdov/Music/Melodify/Leave The Door Open Bruno Mars, Anderson .Paak, Silk Sonic.mp3
+/home/mmdov/Music/Melodify/Pink Pony Club Chappell Roan.mp3
+/home/mmdov/Music/Melodify/Red Wine Supernova Chappell Roan.mp3
+/home/mmdov/Music/Melodify/HOT TO GO Chappell Roan.mp3
+/home/mmdov/Music/Melodify/Good Luck, Babe Chappell Roan.mp3
+/home/mmdov/Music/Melodify/Enter Hallownest Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Dirtmouth Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Crossroads Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/False Knight Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/The White Lady Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Broken Vessel Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Kingdoms Edge Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Nosk Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Dream Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Dream Battle Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/White Palace Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Sealed Vessel Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Radiance Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Hollow Knight Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Enter Pharloom Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Moss Grotto Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Strive Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Bone Bottom Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/The Marrow Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Bell Beast Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Repose Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Deep Docks Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Lace Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Far Fields Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Fourth Chorus Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Greymoor Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Incisive Battle Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Bellhart Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Widow Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Shellwood Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Sister Splinter Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Hunters Trail Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Sinners Road Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Cut Through Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Bilewater Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/The Mist Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Phantom Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/The Slab Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Red Maiden Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Mount Fay Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Blasted Steps Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Last Judge Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Underworks Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Choral Chambers Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Songclave Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Cogwork Dancers Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Cogwork Core Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Whispering Vaults Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Trobbio Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/High Halls Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/The Choir Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Awakening Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Dark Descent Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Reprieve Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Nyleth Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Skarrsinger Karmelita Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Sands of Karak Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Crust King Khann Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Lost Verdania Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Clover Dancers Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Fleatopia Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Tormented Trobbio Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Red Memory Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Last Dive Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Lost Lace Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Sister of the Void Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Silksong Christopher Larkin.mp3
+/home/mmdov/Music/Melodify/Adventure Of A Lifetime Coldplay.mp3
+/home/mmdov/Music/Melodify/The Scientist Coldplay.mp3
+/home/mmdov/Music/Melodify/Paradise Coldplay.mp3
+/home/mmdov/Music/Melodify/Yellow Coldplay.mp3
+/home/mmdov/Music/Melodify/Fix You Coldplay.mp3
+/home/mmdov/Music/Melodify/Get Lucky Radio Edit ft Pharrell Williams Daft Punk.mp3
+/home/mmdov/Music/Melodify/Harder, Better, Faster, Stronger Daft Punk.mp3
+/home/mmdov/Music/Melodify/Veridis Quo Daft Punk.mp3
+/home/mmdov/Music/Melodify/Around the World Daft Punk.mp3
+/home/mmdov/Music/Melodify/Give Life Back to Music Daft Punk.mp3
+/home/mmdov/Music/Melodify/Contact Daft Punk.mp3
+/home/mmdov/Music/Melodify/Motherboard Daft Punk.mp3
+/home/mmdov/Music/Melodify/Within Daft Punk.mp3
+/home/mmdov/Music/Melodify/Giorgio by Moroder Daft Punk.mp3
+/home/mmdov/Music/Melodify/The Game of Love Daft Punk.mp3
+/home/mmdov/Music/Melodify/Get Lucky feat. Pharrell Williams and Nile Rodgers Daft Punk, Pharrell Williams, Nile Rodgers.mp3
+/home/mmdov/Music/Melodify/The Writing of Fragments of Time feat. Todd Edwards Daft Punk, Todd Edwards.mp3
+/home/mmdov/Music/Melodify/The Writing of Fragments of Time feat. Todd Edwards Daft Punk, Todd Edwards1.mp3
+/home/mmdov/Music/Melodify/GLBTM Studio Outtakes Daft Punk.mp3
+/home/mmdov/Music/Melodify/GLBTM Studio Outtakes Edit Daft Punk.mp3
+/home/mmdov/Music/Melodify/GLBTM Studio Outtakes Daft Punk1.mp3
+/home/mmdov/Music/Melodify/Horizon Ouverture Daft Punk.mp3
+/home/mmdov/Music/Melodify/Horizon Japan CD Daft Punk.mp3
+/home/mmdov/Music/Melodify/Infinity Repeating 2013 Demo feat. Julian CasablancasThe Voidz Daft Punk, Julian Casablancas, The Voidz1.mp3
+/home/mmdov/Music/Melodify/Prime 2012 Unfinished Daft Punk.mp3
+/home/mmdov/Music/Melodify/LYTD Vocoder Tests feat. Pharrell Williams Daft Punk, Pharrell Williams.mp3
+/home/mmdov/Music/Melodify/Touch 2021 Epilogue feat. Paul Williams Daft Punk, Paul Williams.mp3
+/home/mmdov/Music/Melodify/Infinity Repeating 2013 Demo feat. Julian CasablancasThe Voidz Daft Punk, Julian Casablancas, The Voidz.mp3
+/home/mmdov/Music/Melodify/Instant Crush feat. Julian Casablancas Daft Punk, Julian Casablancas.mp3
+/home/mmdov/Music/Melodify/Beyond Daft Punk, Leon Bridges.mp3
+/home/mmdov/Music/Melodify/Doin it Right feat. Panda Bear Daft Punk, Panda Bear.mp3
+/home/mmdov/Music/Melodify/Touch feat. Paul Williams Daft Punk, Paul Williams.mp3
+/home/mmdov/Music/Melodify/Lose Yourself to Dance feat. Pharrell Williams Daft Punk, Pharrell Williams.mp3
+/home/mmdov/Music/Melodify/Get Lucky feat. Pharrell Williams  Nile Rodgers Daft Punk, Pharrell Williams, Nile Rodgers.mp3
+/home/mmdov/Music/Melodify/Starboy FT. Daft Punk Daft Punk, The Weeknd.mp3
+/home/mmdov/Music/Melodify/I Feel It Coming feat. Daft Punk Daft Punk, The Weeknd.mp3
+/home/mmdov/Music/Melodify/Fragments of Time feat. Todd Edwards Daft Punk, Todd Edwards.mp3
+/home/mmdov/Music/Melodify/Maison Emilio Piano, Lucie.mp3
+/home/mmdov/Music/Melodify/Delgir Erfan Tahmasbi.mp3
+/home/mmdov/Music/Melodify/Khodahafez Erfan Tahmasbi.mp3
+/home/mmdov/Music/Melodify/To Erfan Tahmasbi.mp3
+/home/mmdov/Music/Melodify/Vay Agar Erfan Tahmasbi.mp3
+/home/mmdov/Music/Melodify/Gnossienne No. 1 Erik Satie, Alena Cherny.mp3
+/home/mmdov/Music/Melodify/Beach Day Frédéric Chopin.mp3
+/home/mmdov/Music/Melodify/Nocturne No 2 In E Flat Major Op 9 No 2 Frédéric Chopin.mp3
+/home/mmdov/Music/Melodify/Nocturne in C-sharp Minor Frédéric Chopin.mp3
+/home/mmdov/Music/Melodify/Like That Future, Metro Boomin, Kendrick Lamar.mp3
+/home/mmdov/Music/Melodify/Gasoline Halsey.mp3
+/home/mmdov/Music/Melodify/Castle Halsey.mp3
+/home/mmdov/Music/Melodify/Him  I Halsey.mp3
+/home/mmdov/Music/Melodify/Haunting Halsey.mp3
+/home/mmdov/Music/Melodify/Colors Halsey.mp3
+/home/mmdov/Music/Melodify/New Americana Halsey.mp3
+/home/mmdov/Music/Melodify/Roman Holiday Halsey.mp3
+/home/mmdov/Music/Melodify/Hurricane Halsey.mp3
+/home/mmdov/Music/Melodify/Ghost Halsey.mp3
+/home/mmdov/Music/Melodify/Control Halsey.mp3
+/home/mmdov/Music/Melodify/Hold Me Down Halsey.mp3
+/home/mmdov/Music/Melodify/Drive Halsey.mp3
+/home/mmdov/Music/Melodify/Strange Love Halsey.mp3
+/home/mmdov/Music/Melodify/Young God Halsey.mp3
+/home/mmdov/Music/Melodify/Eyes Closed Halsey.mp3
+/home/mmdov/Music/Melodify/Allahim Neydi Gunahim İbrahim Tatlıses.mp3
+/home/mmdov/Music/Melodify/It Was A Good Day Ice Cube.mp3
+/home/mmdov/Music/Melodify/Wrecked Imagine Dragons.mp3
+/home/mmdov/Music/Melodify/Amazing INNA.mp3
+/home/mmdov/Music/Melodify/Merry-Go-Round - Piano Solo Ver. Joe Hisaishi.mp3
+/home/mmdov/Music/Melodify/Merry-Go-Round of Life - from Howls Moving Castle Joe Hisaishi.mp3
+/home/mmdov/Music/Melodify/Merry-Go-Round of Life - from Howl’s Moving Castle Joe Hisaishi, Royal Philharmonic Orchestra.mp3
+/home/mmdov/Music/Melodify/Love Me Again John Newman.mp3
+/home/mmdov/Music/Melodify/It’s Over  Engagement Party Justin Hurwitz.mp3
+/home/mmdov/Music/Melodify/Mia  Sebastian’s Theme Justin Hurwitz.mp3
+/home/mmdov/Music/Melodify/Mia Hates Jazz Justin Hurwitz.mp3
+/home/mmdov/Music/Melodify/Epilogue - From La La Land Soundtrack Justin Hurwitz.mp3
+/home/mmdov/Music/Melodify/City Of Stars - From La La Land SoundtrackPier Ryan Gosling.mp3
+/home/mmdov/Music/Melodify/Audition The Fools Who Dream - From La La Land Soundtrack Emma Stone.mp3
+/home/mmdov/Music/Melodify/Planetarium Justin Hurwitz.mp3
+/home/mmdov/Music/Melodify/Summer Montage  Madeline Justin Hurwitz.mp3
+/home/mmdov/Music/Melodify/Armstrong Cabin Justin Hurwitz.mp3
+/home/mmdov/Music/Melodify/euphoria Kendrick Lamar.mp3
+/home/mmdov/Music/Melodify/Not Like Us Kendrick Lamar.mp3
+/home/mmdov/Music/Melodify/luther Kendrick Lamar.mp3
+/home/mmdov/Music/Melodify/All The Stars with SZA Kendrick Lamar, SZA.mp3
+/home/mmdov/Music/Melodify/Strangers Kenya Grace.mp3
+/home/mmdov/Music/Melodify/Die With A Smile Lady Gaga, Bruno Mars.mp3
+/home/mmdov/Music/Melodify/Summertime Sadness Radio Mix Lana Del Rey.mp3
+/home/mmdov/Music/Melodify/Dark Paradise Lana Del Rey.mp3
+/home/mmdov/Music/Melodify/Doin Time Lana Del Rey.mp3
+/home/mmdov/Music/Melodify/Young And Beautiful Lana Del Rey.mp3
+/home/mmdov/Music/Melodify/What A Wonderful World Louis Armstrong.mp3
+/home/mmdov/Music/Melodify/First Movement From Symphony No 5 Ludwig van Beethoven.mp3
+/home/mmdov/Music/Melodify/Sonata No 14 Moonlight Ludwig van Beethoven.mp3
+/home/mmdov/Music/Melodify/Animals Maroon 5.mp3
+/home/mmdov/Music/Melodify/Maps Maroon 5.mp3
+/home/mmdov/Music/Melodify/Moves Like Jagger - Studio Recording From The Voice Performance Maroon 5, Christina Aguilera.mp3
+/home/mmdov/Music/Melodify/Payphone Maroon 5, Wiz Khalifa.mp3
+/home/mmdov/Music/Melodify/Creepin with The Weeknd  21 Savage Metro Boomin, The Weeknd, 21 Savage.mp3
+/home/mmdov/Music/Melodify/Annihilate Spider-Man Across the Spider-Verse Metro Boomin  Swae Lee, Lil Wayne, Offset Metro Boomin, Swae Lee, Lil Wayne.mp3
+/home/mmdov/Music/Melodify/Che Shod Mohsen Chavoshi.mp3
+/home/mmdov/Music/Melodify/Khodkhah Mohsen Yeganeh.mp3
+/home/mmdov/Music/Melodify/Nadaramet Mohsen Yeganeh.mp3
+/home/mmdov/Music/Melodify/Welcome to the Black Parade My Chemical Romance.mp3
+/home/mmdov/Music/Melodify/I Dont Love You My Chemical Romance.mp3
+/home/mmdov/Music/Melodify/Famous Last Words My Chemical Romance.mp3
+/home/mmdov/Music/Melodify/Teenagers My Chemical Romance.mp3
+/home/mmdov/Music/Melodify/Mama My Chemical Romance.mp3
+/home/mmdov/Music/Melodify/Dead My Chemical Romance.mp3
+/home/mmdov/Music/Melodify/This Is How I Disappear My Chemical Romance.mp3
+/home/mmdov/Music/Melodify/Disenchanted My Chemical Romance.mp3
+/home/mmdov/Music/Melodify/Cancer My Chemical Romance.mp3
+/home/mmdov/Music/Melodify/The Sharpest Lives My Chemical Romance.mp3
+/home/mmdov/Music/Melodify/House of Wolves My Chemical Romance.mp3
+/home/mmdov/Music/Melodify/Sleep My Chemical Romance.mp3
+/home/mmdov/Music/Melodify/Better Now Post Malone.mp3
+/home/mmdov/Music/Melodify/Circles Post Malone.mp3
+/home/mmdov/Music/Melodify/Wow. Post Malone.mp3
+/home/mmdov/Music/Melodify/rockstar feat. 21 Savage Post Malone, 21 Savage.mp3
+/home/mmdov/Music/Melodify/Sunflower - Spider-Man Into the Spider-Verse Post Malone, Swae Lee.mp3
+/home/mmdov/Music/Melodify/Goodbyes feat. Young Thug Post Malone, Young Thug.mp3
+/home/mmdov/Music/Melodify/Bohemian Rhapsody Live Queen.mp3
+/home/mmdov/Music/Melodify/Bohemian Rhapsody Queen.mp3
+/home/mmdov/Music/Melodify/Hameye Un Rooza Concert Version Reza Sadeghi.mp3
+/home/mmdov/Music/Melodify/Gamoonam Reza Sadeghi.mp3
+/home/mmdov/Music/Melodify/Khabar Dari Reza Sadeghi.mp3
+/home/mmdov/Music/Melodify/Hame On Rooza Reza Sadeghi.mp3
+/home/mmdov/Music/Melodify/Diamonds Rihanna.mp3
+/home/mmdov/Music/Melodify/APT. ROSÉ, Bruno Mars.mp3
+/home/mmdov/Music/Melodify/City Of Stars Ryan Gosling.mp3
+/home/mmdov/Music/Melodify/Ojos Tristes Selena Gomez, benny blanco.mp3
+/home/mmdov/Music/Melodify/Bi Ehsas Shadmehr Aghili.mp3
+/home/mmdov/Music/Melodify/Khaabe Khosh Shadmehr Aghili.mp3
+/home/mmdov/Music/Melodify/Door Shodi Shadmehr Aghili.mp3
+/home/mmdov/Music/Melodify/Vaghti Ke Bad Misham Shadmehr Aghili.mp3
+/home/mmdov/Music/Melodify/Mamnoon Shadmehr Aghili.mp3
+/home/mmdov/Music/Melodify/Edame Midamet Shadmehr Aghili.mp3
+/home/mmdov/Music/Melodify/Through the Valley Shawn James.mp3
+/home/mmdov/Music/Melodify/The Curse of the Fold Shawn James.mp3
+/home/mmdov/Music/Melodify/Moth To A Flame with The Weeknd Swedish House Mafia, The Weeknd.mp3
+/home/mmdov/Music/Melodify/Kill Bill SZA.mp3
+/home/mmdov/Music/Melodify/Power is Power feat. The Weeknd  Travis Scott SZA, The Weeknd, Travis Scott.mp3
+/home/mmdov/Music/Melodify/All The Things She Said t.A.T.u..mp3
+/home/mmdov/Music/Melodify/Mr. Brightside The Killers.mp3
+/home/mmdov/Music/Melodify/No One Noticed The Marías.mp3
+/home/mmdov/Music/Melodify/In Your Eyes Remix The Weeknd.mp3
+/home/mmdov/Music/Melodify/Blinding Lights The Weeknd.mp3
+/home/mmdov/Music/Melodify/In Your Eyes The Weeknd.mp3
+/home/mmdov/Music/Melodify/Save Your Tears The Weeknd.mp3
+/home/mmdov/Music/Melodify/Die For You - Remix The Weeknd, Ariana Grande.mp3
+/home/mmdov/Music/Melodify/One Of The Girls with JENNIE, Lily Rose Depp The Weeknd, JENNIE, Lily Rose Depp.mp3
+/home/mmdov/Music/Melodify/Save Your Tears with Ariana Grande Remix The Weeknd, Ariana Grande.mp3
+/home/mmdov/Music/Melodify/Audition The Fools Who Dream - Studio Version Emma Stone.mp3
+/home/mmdov/Music/Melodify/I’m Just Ken Ryan Gosling.mp3
