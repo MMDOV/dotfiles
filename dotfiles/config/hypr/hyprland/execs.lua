c66bbdaffc07af1b1eda1b6150dcabfa7006a485
mmdov <mohammadvary@gmail.com>
2026-05-28 16:46:14 +0330

    Automated Commit - 2026-05-28 16:46:14

--- dotfiles/config/hypr/hyprland/execs.lua
+++ dotfiles/config/hypr/hyprland/execs.lua
@@ -31,12 +31,12 @@ hl.on("hyprland.start", function()
 			.. "/.local/bin/tmux-sessionizer $HOME' \\; attach",
 		{ workspace = "3" }
 	)
-	-- hl.exec_cmd("uwsm app -- Telegram", {workspace = "4 silent"})
-	-- hl.exec_cmd("uwsm app -- thunderbird", { workspace = "4 silent" })
-	hl.exec_cmd("uwsm app -- dolphin", { workspace = "4 silent" })
+	hl.exec_cmd("uwsm app -- Telegram", { workspace = "4 silent" })
+	hl.exec_cmd("uwsm app -- thunderbird", { workspace = "4 silent" })
+	-- hl.exec_cmd("uwsm app -- dolphin", { workspace = "4 silent" })
 	hl.exec_cmd("uwsm app -- teamspeak3", { workspace = "5 silent" })
 	hl.exec_cmd("uwsm app -- gio launch " .. homeDir .. "/.local/share/applications/youtube-music.desktop")
-	-- hl.exec_cmd("uwsm app -- vesktop", { workspace = "5 silent" })
+	hl.exec_cmd("uwsm app -- discord", { workspace = "5 silent" })
 	--hl.exec_cmd(
 	--	"uwsm app -- alacritty -e '" .. homeDir .. "/Projects/ponisha-automated/run_main.sh'",
 	--	{ workspace = "6 silent" }
