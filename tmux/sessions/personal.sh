49b49ce8ac9721d6a0004509e39e6a9d25ce1707
mmdov <mohammadvary@gmail.com>
2026-06-15 19:10:35 +0330

    moved to claude code because I hit monthly limit on opencode

--- tmux/sessions/personal.sh
+++ tmux/sessions/personal.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 tmux new-window -dn scratch
-tmux new-window -dn opencode
+tmux new-window -dn claude
 
 tmux send-keys -t scratch "clear" C-m
 
-tmux send-keys -t opencode "opencode" C-m
-tmux send-keys -t opencode "clear" C-m
+tmux send-keys -t claude "claude" C-m
+tmux send-keys -t claude "clear" C-m
 
 clear
 vim .
