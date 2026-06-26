d745e4ef4b0492dafd4cd13776f341f7941290d9
mmdov <mohammadvary@gmail.com>
2026-06-26 16:56:14 +0330

    Automated Commit - 2026-06-26 16:56:14

--- tmux/sessions/personal.sh
+++ tmux/sessions/personal.sh
@@ -2,11 +2,15 @@
 
 tmux new-window -dn scratch
 tmux new-window -dn claude
+tmux new-window -dn opencode
 
 tmux send-keys -t scratch "clear" C-m
 
 tmux send-keys -t claude "claude" C-m
 tmux send-keys -t claude "clear" C-m
+
+tmux send-keys -t opencode "opencode" C-m
+tmux send-keys -t opencode "clear" C-m
 
 clear
 vim .
