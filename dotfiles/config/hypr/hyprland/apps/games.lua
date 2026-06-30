15c0439ad3d553532632cb862e60caf16f7e29e7
mmdov <mohammadvary@gmail.com>
2026-06-30 13:53:12 +0330

    its good enough man idk

--- dotfiles/config/hypr/hyprland/apps/games.lua
+++ dotfiles/config/hypr/hyprland/apps/games.lua
@@ -1,6 +1,6 @@
 hl.window_rule({
 	match = {
-		class = "([Oo]verwatch|(net.)?[Ll]utris(.[Ll]utris)?|[Bb]attle.net(.exe)?|[Hh]earthstone(.exe)?|[Bb]alatro(.exe)?|[Tt]he [Bb]azaar|(com.usebottles.)?[Bb]ottles)",
+		class = "([Oo]verwatch|(net.)?[Ll]utris(.[Ll]utris)?|[Bb]alatro(.exe)?|[Tt]he [Bb]azaar|(com.usebottles.)?[Bb]ottles)",
 	},
 	workspace = "1 silent",
 	opacity = "1",
@@ -8,16 +8,59 @@ hl.window_rule({
 })
 
 hl.window_rule({
-	name = "hdt_overlay",
-	match = {
-		title = "^(Hearthstone Deck Tracker.*)$",
-	},
+	match = { initial_class = "(hearthstone.exe)" },
+	workspace = "1 silent",
+	tile = true,
+})
+
+hl.window_rule({
+	match = { initial_class = "(battle.net.exe)" },
+	workspace = "1 silent",
 	float = true,
-	pin = false,
-	no_focus = false,
+})
+hl.window_rule({
+	match = { initial_class = "(battle.net.exe)", initial_title = "(Battle.net Login)" },
+	workspace = "1 silent",
+	float = true,
+})
+
+hl.window_rule({
+	match = { initial_class = "(battle.net.exe)", initial_title = "(Battle.net)" },
+	center = true,
+	size = { 1100, 700 },
+})
+
+hl.window_rule({
+	match = {
+		initial_class = "(hearthstonedecktracker.exe)",
+		initial_title = "(HearthstoneOverlay)",
+	},
+	workspace = "1 silent",
+	float = true,
+	no_focus = true,
 	no_blur = true,
 	border_size = 0,
 })
+
+hl.window_rule({
+	match = {
+		initial_class = "(hearthstonedecktracker.exe)",
+		initial_title = "([Hh]earthstone [Dd]eck [Tt]racker)",
+	},
+	workspace = "8 silent",
+	float = true,
+	center = true,
+	size = { 1100, 700 },
+})
+
+hl.window_rule({
+	match = {
+		initial_class = "(hearthstonedecktracker.exe)",
+		initial_title = "(SplashScreenWindow)",
+	},
+	workspace = "8 silent",
+	float = true,
+})
+
 hl.window_rule({ match = { class = "ArenaTracker" }, float = true })
-hl.window_rule({ match = { class = "[Bb]attle.net(.exe)?" }, float = true })
-hl.window_rule({ match = { title = "[Bb]attle.net(.exe)?" }, float = true })
+hl.window_rule({ match = { class = "explorer.exe" }, float = true, size = { 162, 25 }, move = "10 40" })
